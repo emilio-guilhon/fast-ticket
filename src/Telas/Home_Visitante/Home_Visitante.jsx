6c7c872c9511d3304b664b26ceabd66b087e65fe
Melquisedeque Cardoso <melquisedeque.lima@aluno.uece.br>
2023-11-11 09:17:51 -0300

    atualizações recentes

--- src/Telas/Home_Visitante/Home_Visitante.jsx
+++ src/Telas/Home_Visitante/Home_Visitante.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Navbar from "../../User/Navbar/Navbar";
 import "./Home_visitante.css"
 
 
-function Home_Visitante(){
+function Home_Visitante() {
+  const [eventos, setEventos] = useState([]);
+
+  useEffect(() => {
+    const fetchEventos = async () => {
+      try {
+        
+        const response = await axios.get("http://localhost:5000/eventos");
+        setEventos(response.data);
+        console.log('dfwfewfwfwewfbwe')
+        console.log('dados: ',response.data);
+      } catch (error) {
+        console.log("Erro na solicitação GET:", error);
+      }
+    };
+
+    fetchEventos();
+  }, []);
     return(
   <div>
     <Navbar/>
      <>
+     
+    
      <div className="body">
       <div className="Eventos">
         <p className="titulo-principal">Eventos em destaque :</p>
