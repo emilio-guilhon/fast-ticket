88c2d92093be8e8f1dd9236359b9f204419515cd
Melquisedeque Cardoso <melquisedeque.lima@aluno.uece.br>
2023-11-12 20:00:49 -0300

    telas cadastro e login atualizadas

--- src/Telas/HomeCliente/HomeCliente.jsx
+++ src/Telas/HomeCliente/HomeCliente.jsx
@@ -7,6 +7,22 @@ function HomeCliente() {
     <div>
          <>
         <NavbarCliente/>
+        <>
+     <div className="body">
+      <div className="Eventos">
+        <p className="titulo-principal">Eventos em destaque :</p>
+         <div className="Evento-principal">  
+         </div>
+        <p className="titulo-demaisEventos">Demais eventos:</p>
+         <div className="Demais-eventos">
+            <div className="x"></div>
+            <div className="y"></div>
+            <div className="z"></div>
+            
+         </div>
+      </div>
+     </div>
+     </>
      </>
     </div>
   )
