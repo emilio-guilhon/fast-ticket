509083ba8d6173d0d7739fc6322445c9ce85c06c
Melquisedeque Cardoso <melquisedeque.lima@aluno.uece.br>
2023-11-12 18:47:08 -0300

    cadastro e login , telas atualizadas

--- src/User/NavbarCliente/NavbarCliente.jsx
+++ src/User/NavbarCliente/NavbarCliente.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import imagem from '../Pictures/image 20.png';
+import iconImg from '../Pictures/image 22.png';
+import "./NavbarCliente.css";
 
 function NavbarCliente() {
-  return (
-    <div>
-         <nav className="navbar-body">
+    return (
+      <div>
+        <nav className="navbar-body">
           <ul>
             <li className="Logo">
-                <button>FastTicket.com</button>
+              <button>FastTicket.com</button>
             </li>
-          <li className="pesquisa">
-          <input type="text" placeholder="..." className="texto-pesquisar" />
-           </li>
-            <li>
-              <button className='Perfil'>Teste de Perfil</button>
+            <li className="pesquisa">
+              <input type="text" placeholder="..." className="texto-pesquisar" />
+            </li>
+            <li className='Perfil'>
+              <img src={imagem} alt="Imagem" className='imagemFt' />
+              <button className='perfilInfo'>Informações do Usuário</button>
+              <img src={iconImg} alt='mostrarMais' className='mostrarMais'/>
             </li>
           </ul>
         </nav>
-    </div>
-  )
-}
-
-export default NavbarCliente
+      </div>
+    );
+  }
+  
+  export default NavbarCliente;
+  
